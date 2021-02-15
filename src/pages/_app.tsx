c7700333d819194ad30f24c6e484dd351a874e9b
AjayLiu <ajayliu2928@gmail.com>
2021-02-15 15:55:23 -0800

    start fresh again

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type {AppProps} from 'next/app'
-
-import '@styles/globals.css'
-function Application({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default Application

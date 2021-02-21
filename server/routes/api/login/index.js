7f4a84eb4a286f57077029ec0068d82b3d63a5a6
AjayLiu <ajayliu2928@gmail.com>
2021-02-21 10:35:50 -0800

    dunno why heroku is dead again

--- server/routes/api/login/index.js
+++ server/routes/api/login/index.js
@@ -1,9 +1,8 @@
 const login = require('express').Router();
-const pool = require('../../../db')
+// const pool = require('../../../db')
 
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { createLogicalAnd } = require('typescript');
 
 require("dotenv").config()
 
@@ -41,20 +40,6 @@ login.get('/google/callback',
     }
 );
 
-// quotes.post("/", async(req, res) => {
-//     try {
-//         const { quote_content } = req.body;
-//         const newQuote = await pool.query(
-//             "INSERT INTO quotes (quote_content) VALUES ($1) RETURNING *;",
-//             [quote_content]
-//         )
-        
-//         res.json(newQuote.rows[0]);
-//     } catch (err) {
-//         console.error(err.message)
-//     }
-// })
-    
 login.get("/", async(req, res) => {
     res.json("Hi");
 })

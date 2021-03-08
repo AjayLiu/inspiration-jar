6cdf6e24983c4e7b43f78af2290e4f1caf7e54cb
AjayLiu <ajayliu2928@gmail.com>
2021-03-07 21:16:21 -0800

    working search bar

--- client/src/components/home/Browse.tsx
+++ client/src/components/home/Browse.tsx
@@ -1,5 +1,8 @@
 import QuoteCard from "@components/QuoteCard";
 import { Quote, Vote } from "@hooks/quoteTypes";
+import styles from "@styles/Browse.module.css";
+import Fuse from "fuse.js";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   quotesList: Array<Quote>;
@@ -7,11 +10,45 @@ interface Props {
 }
 
 const Browse: React.FC<Props> = (props) => {
-  const quotesList = props.quotesList;
+  const [quotesList, setQuotesList] = useState(props.quotesList);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    setQuotesList(props.quotesList);
+  }, [props.quotesList]);
+
+  let options = {
+    keys: ["quoteContent"],
+    threshold: 0.2,
+  };
+
+  const fuse = new Fuse(quotesList, options);
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchVal = e.target.value;
+    setSearchText(searchVal);
+    if (searchVal != "") {
+      const filterResult = fuse.search(searchVal);
+
+      const newList = filterResult.map((obj) => {
+        return obj.item;
+      });
+      console.log(newList);
+      setQuotesList(newList);
+    } else {
+      setQuotesList(props.quotesList);
+    }
+  };
 
   return (
     <div>
       <h2>Browse</h2>
+      <input
+        className={styles.searchBar}
+        type="text"
+        value={searchText}
+        placeholder="Search..."
+        onChange={(e) => onSearchChange(e)}
+      />
       {quotesList.map((item, idx) => {
         let voted = false;
         props.votesList.forEach((obj) => {
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,43 +1,51 @@
-import Head from 'next/head'
-import Footer from '@components/Footer'
-import GoogleAnalyticsHook from '@components/GoogleAnalyticsHook'
-import LandingPage from '@components/home/LandingPage'
-import Navbar from '@components/Navbar'
-import DrawAndBrowse from '@components/home/DrawAndBrowse'
+import Footer from "@components/Footer";
+import GoogleAnalyticsHook from "@components/GoogleAnalyticsHook";
+import DrawAndBrowse from "@components/home/DrawAndBrowse";
+import LandingPage from "@components/home/LandingPage";
+import Navbar from "@components/Navbar";
+import Head from "next/head";
 
 const Home: React.FC = () => {
   return (
     <>
-    <GoogleAnalyticsHook />
-    <div className="container">
-      <Head>
-        <title>Inspiration Jar</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="Description"
-          content="Share words of encouragement to fellow humans around the 
+      <GoogleAnalyticsHook />
+      <div className="container">
+        <Head>
+          <title>Inspiration Jar</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="Description"
+            content="Share words of encouragement to fellow humans around the 
           world!"
-        /> 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Inspiration Jar" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://inspirationjar.herokuapp.com/" />
-        <meta property="og:image" content="https://inspirationjar.herokuapp.com/img/jar.png" />
-        <meta property="og:description" content="Share words of encouragement to fellow humans around the 
-          world!" />
-      </Head>
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content="Inspiration Jar" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://inspirationjar.herokuapp.com/"
+          />
+          <meta
+            property="og:image"
+            content="https://inspirationjar.herokuapp.com/img/jar.png"
+          />
+          <meta
+            property="og:description"
+            content="Share words of encouragement to fellow humans around the 
+          world!"
+          />
+        </Head>
 
-      <main>
-        <Navbar />
-        <LandingPage />
-        <DrawAndBrowse />
-      </main>
+        <main>
+          <Navbar />
+          <LandingPage />
+          <DrawAndBrowse />
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
     </>
-  )
-}
-
+  );
+};
 
 export default Home;

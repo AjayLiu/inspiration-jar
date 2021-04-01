8161019bf14ab92f3cda096307faf56afb468281
AjayLiu <ajayliu2928@gmail.com>
2021-03-31 23:17:17 -0700

    css to scss nesting refactor

--- client/src/components/LoginButton/LoginButton.tsx
+++ client/src/components/LoginButton/LoginButton.tsx
@@ -1,9 +1,8 @@
-import styles from "./LoginButton.module.scss";
 const LoginButton: React.FC = () => {
   return (
     <div>
       <a href="/api/login/google">
-        <img className={styles.image} src="img/google-login.png"></img>
+        <img src="img/google-login.png"></img>
       </a>
     </div>
   );

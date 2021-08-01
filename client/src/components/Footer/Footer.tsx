1fd8bce50cfedb4a28a533c76cb707a14e918d49
AjayLiu <ajayliu2928@gmail.com>
2021-07-31 21:20:55 -0700

    Add 'view project on Github' to Footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Footer.module.scss";
 const Footer: React.FC = () => {
   const imgPath = "https://ajayliu.com/img";
+  const projectRepoLink = "https://github.com/AjayLiu/inspiration-jar";
   return (
     <>
       <footer className={styles.footer}>
@@ -24,6 +25,12 @@ const Footer: React.FC = () => {
           </a>
           <a href="https://www.youtube.com/channel/UClr6XCaguPeM0g7UL6Lvs3g">
             <img src={imgPath + "/youtube.svg"} alt="youtube logo" />
+          </a>
+        </div>
+        <div className={styles.repo}>
+          <a href={projectRepoLink} className={styles.repoAnchor}>
+            <img src={imgPath + "/repo.svg"} />
+            View this project on Github
           </a>
         </div>
       </footer>
